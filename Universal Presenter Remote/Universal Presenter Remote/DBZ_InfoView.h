0d40feee2e49e880579e0f0396e86d13ee58d80e
Brendan Boyle <brendancboyle@me.com>
2014-04-20 14:25:00 -0500

    Added instructions

---
+++ Universal Presenter Remote/Universal Presenter Remote/DBZ_InfoView.h
@@ -0,0 +1,15 @@
+//
+//  DBZ_InfoView.h
+//  Universal Presenter Remote
+//
+//  Created by Brendan Boyle on 4/20/14.
+//  Copyright (c) 2014 DBZ Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBZ_InfoView : UIViewController
+
+- (IBAction)close:(id)sender;
+
+@end
---
+++ Universal Presenter Remote/Universal Presenter Remote/DBZ_InfoView.m
@@ -0,0 +1,55 @@
+//
+//  DBZ_InfoView.m
+//  Universal Presenter Remote
+//
+//  Created by Brendan Boyle on 4/20/14.
+//  Copyright (c) 2014 DBZ Technology. All rights reserved.
+//
+
+#import "DBZ_InfoView.h"
+#import <iAd/iAd.h>
+
+@interface DBZ_InfoView ()
+
+@end
+
+@implementation DBZ_InfoView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.canDisplayBannerAds = YES;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)close:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
