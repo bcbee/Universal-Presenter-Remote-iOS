c5cb94ab790a709a1169058f69e77d1a01d9cd32
Brendan Boyle <brendancboyle@me.com>
2014-08-26 20:32:38 -0400

    More robust solution for nav logo

--- Universal Presenter Remote/Universal Presenter Remote/DBZ_LoginView.m
+++ Universal Presenter Remote/Universal Presenter Remote/DBZ_LoginView.m
@@ -28,13 +28,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-    {
-        [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"iPadNav"] forBarMetrics:UIBarMetricsDefault];
-    } else {
-        [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"iPhoneNav"] forBarMetrics:UIBarMetricsDefault];
-    }
+    [self.navigationController.navigationBar setTitleTextAttributes: @{NSForegroundColorAttributeName: [UIColor whiteColor], NSFontAttributeName: [UIFont fontWithName:@"BatmanForeverAlternate" size:35.0f]}];
     self.canDisplayBannerAds = YES;
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateInterface:) name:@"UpdateInterface" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshInterface:) name:@"Refresh" object:nil];
