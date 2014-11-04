a5b143786d4c7ed443de3695e9e86fa84d33bed8
Brendan Boyle <brendancboyle@me.com>
2014-11-04 13:10:00 -0500

    Added basic QR processing script and interface

---
+++ Universal Presenter Remote/Universal Presenter Remote/igViewController.h
@@ -0,0 +1,13 @@
+//
+//  igViewController.h
+//  ScanBarCodes
+//
+//  Created by Torrey Betts on 10/10/13.
+//  Copyright (c) 2013 Infragistics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface igViewController : UIViewController
+
+@end
